41a9cd1373c249abf12981c0a13adbc160379349
Provoski <provo.iyen27@gmail.com>
2022-09-14 08:52:08 -0700

    task 0 added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * prt - print _putchar
+ * Description: it prints "_putchar" using _putchar command to std
+ * Return: void
+ */
+void prt(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+/**
+ * main - master function
+ * Description: entry point for other function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	prt();
+	return (0);
+}
