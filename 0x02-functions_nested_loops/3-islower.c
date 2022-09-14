4fff44f69c976547637fb0fd42e7a19a4652b844
Provoski <provo.iyen27@gmail.com>
2022-09-14 12:43:12 -0700

    task 0 first correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,9 @@
+/**
+ * _islower - check lower alphabet
+ * Description: check if inputed letter is lower or capital
+ * @c: value to check
+ * Return: 0 if capital or 1 if lower
+ */
 int _islower(int c)
 {
 	if (c < 'a' || c > 'z')
