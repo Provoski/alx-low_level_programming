9ef878b3e072c3c63de84d81a4f3e60a8d6ab303
Provoski <provo.iyen27@gmail.com>
2022-09-14 11:49:13 -0700

    task 2 added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - print aphabet in lower case
+ * Return: 0 on success
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c + 0);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
