f5e53d982ea70d4767d6946d8510bd8dd829768d
Provoski <provo.iyen27@gmail.com>
2022-09-19 15:08:11 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+include "main.h"
+/**
+ * reset_to_98 - reset the value of a variable
+ * with another value
+ * @*n: a pointer variable
+ * Return: bo return value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - master function
+ * Description: entry point for other function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	
+	return (0);
+}
