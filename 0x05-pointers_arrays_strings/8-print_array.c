acedc798cc175e84d107752d229e54b5a9bbe84c
Provoski <provo.iyen27@gmail.com>
2022-09-19 21:05:45 -0700

    task 8 correctiob

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("arr[%d] = %d\n", i, *a++);
+		printf("%d", *a++);
+		if (i < n-1)
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
+	printf("\n");
 }
