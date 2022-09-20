d28d759378d78a4ba2806e5b88912f3a9b643e4e
Provoski <provo.iyen27@gmail.com>
2022-09-20 01:00:27 +0000

    task added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,5 +9,5 @@
 
 void print_rev(char *s)
 {
-	strsep(s);
+	printf("%s\n", strrev(s));
 }
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -11,7 +11,7 @@ int main(void)
 	char *str;
  
 	str = "my man";
-	_puts(str);
+	print_rev(str);
 
 	return (0);
 }
