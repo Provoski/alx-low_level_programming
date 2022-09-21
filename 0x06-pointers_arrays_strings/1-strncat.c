f36291785b8742899752748191f14570048a07b7
Provoski <provo.iyen27@gmail.com>
2022-09-21 17:07:55 +0000

    task 1 correcion

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	char *con_string;
-	int lenght;
 
-	lenght = strlen(src);
-	if (*src == '\0' || n == lenght || n > lenght)
-	{
-		return (src);
-	}
-	else
 	con_string = strncat(dest, src, n);
 	return (con_string);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,18 +7,19 @@
 
 int main(void)
 {
-	char s1[98] = "hello";
-	char s2[] = "world!";
-	char s3[10] = " melody";
+	char s1[98] = "hello ";
+	char s2[] = "world!\n";
 	char *ptr;
 
 	printf("%s\n", s1);
-	printf("%s\n", s2);
-	ptr = _strncat(s1,s2,3);
+	printf("%s", s2);
+	ptr = _strncat(s1,s2,1);
+	printf("%s\n", s1);
+	printf("%s", s2);
 	printf("%s\n", ptr);
 	ptr = _strncat(s1, s2, 1024);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s3, 4);
-	printf("%s\n", ptr);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
 	return (0);
 }
