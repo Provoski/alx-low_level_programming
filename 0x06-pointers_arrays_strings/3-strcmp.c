7bbc94413bbcd1d4e1ea882605d576f292796cc7
Provoski <provo.iyen27@gmail.com>
2022-09-21 19:33:32 +0000

    task 3 added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - concatenate strings
+ * @s1: variable for first string
+ * @s2: variable for second string
+ * Return: com_string
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	int *con_string;
+
+	com_string = strcmp(s1, s2);
+	return (com_string);
+}
