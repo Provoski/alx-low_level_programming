72594ded3aeb2870affceed2f42e5fd0b9718946
Provoski <provo.iyen27@gmail.com>
2022-09-26 05:02:17 -0700

    task 0 first correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,5 +12,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s, b, n));
+	unsigned int index;
+	unsigned char *memory;
+	unsigned char value;
+
+	memory = s;
+	value = b;
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
 }
