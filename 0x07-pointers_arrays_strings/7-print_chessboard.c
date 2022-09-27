88cb64e44c1c960399fff0ce7c917c8ba6f3755d
Provoski <provo.iyen27@gmail.com>
2022-09-26 17:27:43 -0700

    task 6 added

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_chessboard - prints the arrangement of a cheesboard
+ * @a: array variable
+ * Return: cheesboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int x;
+
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (x = 0; x < 8; x++)
+		printf("%c", a[i][x]);
+		printf("\n");
+	}
+}
