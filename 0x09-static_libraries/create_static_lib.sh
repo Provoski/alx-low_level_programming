557412bb2cffcb9165ea4de8b9ed2ff7993ff4a4
Provoski <provo.iyen27@gmail.com>
2022-10-01 09:37:09 +0000

    bash file for creating static libraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -c -pedantic -Wall -Wextra -Werror -std=gnu89 *.c | ar -rc liball.a *.o | ranlib liball.a
+gcc -c -pedantic -Wall -Wextra -Werror -std=gnu89 *.c
+ar -rc liball.a *.o
+ranlib liball.a
