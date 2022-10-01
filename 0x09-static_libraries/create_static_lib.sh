d908a3c181eeec8fc20fdb501d7b46066f40ca1f
Provoski <provo.iyen27@gmail.com>
2022-09-30 23:41:18 -0700

    correction

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c -pedantic -Wall -Wextra -Werror -std=gnu89 *.c | ./ar -rc liball.a *.o | ./ranlib liball.a
+gcc -c -pedantic -Wall -Wextra -Werror -std=gnu89 *.c | ar -rc liball.a *.o | ranlib liball.a
