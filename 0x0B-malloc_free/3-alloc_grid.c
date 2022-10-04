37ff20ba49e366735721b04756559bdb6248c220
Provoski <provo.iyen27@gmail.com>
2022-10-04 15:29:13 -0700

    task 3 and 4 in malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - return a pointer to a two dimension of array
+ * @height: array number of row
+ * @width: array number of element per row
+ * Return: NULL on failure or if width or height is 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid_alloc;
+	int width_index;
+	int height_index;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid_alloc = (int **) malloc(sizeof(int) * height);
+	if (grid_alloc == NULL)
+	{
+		return (NULL);
+	}
+	for (height_index = 0; height_index < height; height_index++)
+	{
+		grid_alloc[height_index] = (int *) malloc(sizeof(int) * width);
+		if (grid_alloc[height_index] == NULL)
+		{
+			for (; height_index >= 0; height_index--)
+			{
+				free(grid_alloc[height_index]);
+			}
+			free(grid_alloc);
+			return (NULL);
+		}
+	}
+	for (height_index = 0; height_index < height; height_index++)
+	{
+		for (width_index = 0; width_index < width; width_index++)
+		grid_alloc[height_index][width_index] = 0;
+	}
+	return (grid_alloc);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - a function that free memory space allocated for a 
+ * two dimensional array
+ * @grid: array row memory size 
+ * @height: array column memory size
+ * Return: Non
+ */
+
+void free_grid(int **grid, int height)
+{
+	int row_index;
+
+	for (row_index = 0; row_index < height; row_index++)
+	{
+		free(grid[row_index]);
+		free(grid);
+	}
+}
