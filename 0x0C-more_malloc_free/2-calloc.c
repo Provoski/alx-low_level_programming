933d304b50c2db4fa1394f517aa5a86e891aa071
Provoski <provo.iyen27@gmail.com>
2022-10-08 12:50:34 +0000

    task 2 on more malloc correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,8 @@
 #include <limits.h>
 
 /**
- * _calloc a function that allocate memory using of specific size using malloc
+ * _calloc -  a function that allocate memory using
+ * of specific size using malloc
  * @nmemb: bytes of memory to allocate
  * @size: size of data type
  * Return: pointer to allocated memory, or NULL on Error
@@ -12,7 +13,9 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *mem;
+	unsigned int index;
 
+	index = 0;
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
@@ -21,6 +24,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (mem == NULL)
 	{
 		return (NULL);
+	}
+	while (mem[index])
+	{
+		mem[index] = 0;
+		index++;
 	}
 	return (mem);
 }
