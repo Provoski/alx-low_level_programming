2e8c26acde8b4a8df6b7714b14af7a1d62c40ad9
Provoski <provo.iyen27@gmail.com>
2022-10-10 15:16:47 -0700

    task 0 on structure added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a data types that hold members or
+ * dog properts of different types
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: this struct data type contains three members
+ * and objects can be created from it
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* #ifndef DOG_H */
