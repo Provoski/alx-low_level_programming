d5ce054571a1f6c97edd0cba0f616790afd05e56
Provoski <provo.iyen27@gmail.com>
2022-10-10 23:32:29 +0000

    task 1 added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,15 +4,15 @@
 /**
  * init_dog - a function that initiallize struct member
  * @d: a struct dog data type
- * @name:name variable 
+ * @name:name variable
  * @age: age variable
  * @owner: owner variable
  * Return: Nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	d -> name = name;
-	d -> age = age;
-	d - owner = owner;
+{		
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
