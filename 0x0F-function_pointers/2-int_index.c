b180bfdcae3a9f3435daee94fda9750c53eba9c4
Provoski <provo.iyen27@gmail.com>
2022-10-12 19:24:46 -0700

    task 2 correction on funtion pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,6 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	temp = 0;
 	flag = 0;
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (index = 0; index < size; index++)
