50822bda2bd76463b3c652af300f0903ba81406b
Provoski <provo.iyen27@gmail.com>
2022-10-14 16:23:09 -0700

    checking repo connection

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,7 +13,6 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int x;
 
-
 	for (i = 0; a[i][7]; i++)
 	{
 		for (x = 0; x < 8; x++)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * main - code checker
  * @argc: argument counter
@@ -9,30 +10,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
 	int val1;
 	int val2;
-	char *op;
-	char array_op[6] = {'+', '-', '*', '/', '%'};
 
-	val1 = atoi(argv[1]);
-	val2 = atoi(argv[3]);
-	op = argv[2];
-	if (argc < 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (op != array_op)
+	val1 = atoi(argv[1]);
+	val2 = atoi(argv[3]);
+	if ()
+	{
+		if ((**(argv + 2) == 37  && val2 == 0) || (val2 == 0 && **(argv + 2) == 47))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		else
+		{
+			printf("%d\n", 9 + 10);
+		}
+	}
+	
+	else
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (val2 == 0 && *op == '%' || *op == '/')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(op)(val1, val2));
 	return (0);
 }
