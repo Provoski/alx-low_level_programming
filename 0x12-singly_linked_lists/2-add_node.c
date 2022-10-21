5affc5cb3d2696e9fcd8d99b52901432daa3a645
Provoski <provo.iyen27@gmail.com>
2022-10-20 19:21:07 -0700

    task 2 added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - a functiin that had node to a the end of a list
+ * @head: the head of the list
+ * @str: string to be duplicated
+ * Return: address of new element or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t *));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
