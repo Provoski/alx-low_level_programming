c12bafdbb468112d7122ce1195fa07ec590f91e7
Provoski <provo.iyen27@gmail.com>
2023-01-06 03:49:15 -0800

    task 0 and task 1 added on done on hash_tables task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table of @size
+ * @size: size of hash table
+ * Return: pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_node_t) * size);
+	if (!hash_table)
+		return (NULL);
+	else
+		return (hash_table);
+}
