28ed6471d484c39b2d088bc55947fa125dc0882e
Provoski <provo.iyen27@gmail.com>
2023-01-06 14:26:23 -0800

    task 0 correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 
 	hash_table = NULL;
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	hash_table = malloc(sizeof(hash_node_t *) * size);
 	if (!hash_table->array)
 		return (NULL);
 	hash_table->size = size;
